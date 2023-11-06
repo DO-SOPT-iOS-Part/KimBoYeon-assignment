68696d213d562f36eccf2b624d8e8346266bfa15
boyeon <judy010119@gmail.com>
2023-11-06 16:22:50 +0900

    [Fix] 데이터 위치 수정

--- weatherApp/weatherApp/Presentation/List/weatherCardItemData.swift
+++ weatherApp/weatherApp/Presentation/List/weatherCardItemData.swift
@@ -1,5 +1,5 @@
 //
-//  weatherCardData.swift
+//  weatherCardItemData.swift
 //  weatherApp
 //
 //  Created by 김보연 on 11/2/23.
@@ -7,20 +7,82 @@
 
 import Foundation
 
-struct WeekWeatherItemData {
-    let dayOfTheWeekLabel: String
-    let weatherIconImageView: String
-    let rainingPercentLabel: String
-    let dayOfMinimumTemperatureLabel: String
-    let graphImageView: String
-    let dayOfMaximumTemperatureLabel: String
+struct WeatherCardItemData {
     
-    init(dayOfTheWeekLabel: String, weatherIconImageView: String, rainingPercentLabel: String, dayOfMinimumTemperatureLabel: String, graphImageView: String, dayOfMaximumTemperatureLabel: String) {
-        self.dayOfTheWeekLabel = dayOfTheWeekLabel
-        self.weatherIconImageView = weatherIconImageView
-        self.rainingPercentLabel = rainingPercentLabel
-        self.dayOfMinimumTemperatureLabel = dayOfMinimumTemperatureLabel
-        self.graphImageView = graphImageView
-        self.dayOfMaximumTemperatureLabel = dayOfMaximumTemperatureLabel
+    let myLocationLabel: String
+    let myLocationNameLabel: String
+    let myLocationConditionLabel: String
+    let myLocationAverageTemperatureLabel: String
+    let myLocationMinimumTemperatureLabel: String
+    let myLocationMaximumTemperatureLabel: String
+    
+    init(myLocationLabel: String, myLocationNameLabel: String, myLocationConditionLabel: String, myLocationAverageTemperatureLabel: String, myLocationMinimumTemperatureLabel: String, myLocationMaximumTemperatureLabel: String) {
+        self.myLocationLabel = myLocationLabel
+        self.myLocationNameLabel = myLocationNameLabel
+        self.myLocationConditionLabel = myLocationConditionLabel
+        self.myLocationAverageTemperatureLabel = myLocationAverageTemperatureLabel
+        self.myLocationMinimumTemperatureLabel = myLocationMinimumTemperatureLabel
+        self.myLocationMaximumTemperatureLabel = myLocationMaximumTemperatureLabel
     }
 }
+
+var weatherCardItemData: [WeatherCardItemData] = [
+    
+    .init(myLocationLabel: "나의 위치",
+          myLocationNameLabel: "고양시",
+          myLocationConditionLabel: "흐림",
+          myLocationAverageTemperatureLabel: "21°",
+          myLocationMinimumTemperatureLabel: "최저:15°",
+          myLocationMaximumTemperatureLabel: "최고:29°"),
+    
+    .init(myLocationLabel: "나의 위치",
+          myLocationNameLabel: "의정부시",
+          myLocationConditionLabel: "흐림",
+          myLocationAverageTemperatureLabel: "27°",
+          myLocationMinimumTemperatureLabel: "최저:13°",
+          myLocationMaximumTemperatureLabel: "최고:25°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "노원시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "25°",
+              myLocationMinimumTemperatureLabel: "최저:11°",
+              myLocationMaximumTemperatureLabel: "최고:23°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "부산광역시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "20°",
+              myLocationMinimumTemperatureLabel: "최저:9°",
+              myLocationMaximumTemperatureLabel: "최고:18°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "고양시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "21°",
+              myLocationMinimumTemperatureLabel: "최저:15°",
+              myLocationMaximumTemperatureLabel: "최고:29°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "의정부시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "27°",
+              myLocationMinimumTemperatureLabel: "최저:13°",
+              myLocationMaximumTemperatureLabel: "최고:25°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "노원시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "25°",
+              myLocationMinimumTemperatureLabel: "최저:11°",
+              myLocationMaximumTemperatureLabel: "최고:23°"),
+    
+        .init(myLocationLabel: "나의 위치",
+              myLocationNameLabel: "부산광역시",
+              myLocationConditionLabel: "흐림",
+              myLocationAverageTemperatureLabel: "20°",
+              myLocationMinimumTemperatureLabel: "최저:9°",
+              myLocationMaximumTemperatureLabel: "최고:18°")
+    
+]
+
