ec818c04e30605510003e0431c5214791b12732e
boyeon <judy010119@gmail.com>
2023-11-10 16:51:10 +0900

    [Add] 수평스크롤 셀 추가

--- weatherApp/weatherApp/Presentation/Detail/dayWeatherCollectionViewCell.swift
+++ weatherApp/weatherApp/Presentation/Detail/dayWeatherCollectionViewCell.swift
@@ -12,6 +12,77 @@ import Then
 
 class dayWeatherCollectionViewCell: UICollectionViewCell {
     
+    private let todayTimeLabel = UILabel()
+    private let todayWeatherImageView = UIImageView()
+    let config = UIImage.SymbolConfiguration(font: .medium(size: 26))
+    private let todayTemperatureLabel = UILabel()
     
+    static let identifier: String = "dayWeatherCollectionViewCell"
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setStyle()
+        self.setLayout()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func bindData(data: DayWeatherItemData) {
+        self.todayTimeLabel.text = data.todayTimeLabel
+        self.todayWeatherImageView.image = UIImage(systemName: data.todayWeatherImageView)
+        self.todayTemperatureLabel.text = data.todayTemperatureLabel
+    }
+    
+    func setStyle() {
+        
+        contentView.do {
+            $0.backgroundColor = .clear
+        }
+        
+
+        todayTimeLabel.do {
+            $0.textColor = .white
+            $0.font = .medium(size: 17)
+            
+        }
+        
+        todayWeatherImageView.do {
+            $0.tintColor = .white
+        }
+        
+        todayTemperatureLabel.do {
+            $0.textColor = .white
+            $0.font = .medium(size: 22)
+        }
+    }
+    
+    func setLayout() {
+        
+        contentView.addSubViews(todayTimeLabel, todayWeatherImageView, todayTemperatureLabel)
+    
+        
+        todayTimeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.height.equalTo(22)
+            $0.centerX.equalToSuperview()
+        }
+        
+        todayWeatherImageView.snp.makeConstraints {
+            $0.top.equalTo(todayTimeLabel.snp.bottom).offset(24)
+            $0.height.equalTo(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+        todayTemperatureLabel.snp.makeConstraints {
+            $0.top.equalTo(todayWeatherImageView.snp.bottom).offset(24)
+            $0.centerX.equalToSuperview()
+        }
+        
+    }
     
 }
