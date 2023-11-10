c030da5eb176428ad252bf94cb3d5e807bf2434c
boyeon <judy010119@gmail.com>
2023-11-10 16:53:43 +0900

    [Fix] 전체 컬뷰에 섹션별 셀 추가 및 수정

--- weatherApp/weatherApp/Presentation/Detail/DetailViewController.swift
+++ weatherApp/weatherApp/Presentation/Detail/DetailViewController.swift
@@ -14,24 +14,27 @@ import SnapKit
 class DetailViewController: UIViewController {
     
     private let backgroundView = UIImageView()
-    private let totalTableView = UITableView(frame: .zero, style: .plain)
+    
+    private let detailCollectionView: UICollectionView = {
+        
+        let mainFlowLayout = UICollectionViewFlowLayout()
+        mainFlowLayout.scrollDirection = .vertical
+        let detailCollectionView = UICollectionView(frame: .zero, collectionViewLayout: mainFlowLayout)
+        return detailCollectionView
+    }()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.isHidden = true
         
+        self.setDetailCollectionViewConfig()
         self.setStyle()
         self.setLayout()
-        self.setTableViewConfig()
         
-    }
-    
-    private func setTableViewConfig() {
-        self.totalTableView.register(topMyLocationWeatherTableViewCell.self,
-                                     forCellReuseIdentifier: topMyLocationWeatherTableViewCell.identifier)
-        self.totalTableView.delegate = self
-        self.totalTableView.dataSource = self
+        
     }
     
 }
@@ -46,51 +49,180 @@ private extension DetailViewController {
             $0.contentMode = .scaleAspectFit
         }
         
-        totalTableView.do {
-            $0.allowsSelection = true
-            $0.isScrollEnabled = true
+        detailCollectionView.do {
             $0.backgroundColor = .clear
         }
+        
     }
     func setLayout() {
         
-        view.addSubViews(backgroundView, totalTableView)
+        view.addSubViews(backgroundView, detailCollectionView)
         
-        totalTableView.snp.makeConstraints() {
+        detailCollectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
     }
     
+    private func setDetailCollectionViewConfig() {
+        
+        self.detailCollectionView.register(topMyLocationWeatherCollectionViewCell.self,
+                                           forCellWithReuseIdentifier: topMyLocationWeatherCollectionViewCell.identifier)
+        
+        self.detailCollectionView.register(dayWeatherCollectionViewHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: dayWeatherCollectionViewHeaderView.identifier)
+        
+        
+        self.detailCollectionView.register(dayWeekHorizontalCollectionViewCell.self,
+                                           forCellWithReuseIdentifier: dayWeekHorizontalCollectionViewCell.identifier)
+        
+        
+        self.detailCollectionView.register(weekWeatherCollectionViewHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: weekWeatherCollectionViewHeaderView.identifier)
+        
+        self.detailCollectionView.register(weekWeatherCollectionViewCell.self,
+                                           forCellWithReuseIdentifier: weekWeatherCollectionViewCell.identifier)
+        
+        self.detailCollectionView.register(weekWeatherCollectionViewFooterView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: weekWeatherCollectionViewFooterView.identifier)
+        
+        self.detailCollectionView.delegate = self
+        self.detailCollectionView.dataSource = self
+    }
 }
 
-extension DetailViewController: UITableViewDelegate {}
-extension DetailViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
+extension DetailViewController: UICollectionViewDelegate {}
+extension DetailViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch section {
         case 0:
             return 1
+        case 1:
+            return 1
+        case 2:
+            return weekWeatherItemData.count
         default:
             return 0
+            
         }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func collectionView(_ listCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         switch indexPath.section {
+            
         case 0:
-            guard let cell = totalTableView.dequeueReusableCell(withIdentifier: topMyLocationWeatherTableViewCell.identifier, for: indexPath) as? topMyLocationWeatherTableViewCell else {return UITableViewCell()}
-            cell.backgroundColor = .clear
-            return cell
+            guard let item = detailCollectionView.dequeueReusableCell(withReuseIdentifier: topMyLocationWeatherCollectionViewCell.identifier, for: indexPath) as? topMyLocationWeatherCollectionViewCell else {return UICollectionViewCell()}
+            return item
+            
+        case 1:
+            guard let item = detailCollectionView.dequeueReusableCell(withReuseIdentifier: dayWeekHorizontalCollectionViewCell.identifier, for: indexPath) as? dayWeekHorizontalCollectionViewCell else {return UICollectionViewCell()}
+            return item
+            
+        case 2:
+            guard let item = detailCollectionView.dequeueReusableCell(withReuseIdentifier: weekWeatherCollectionViewCell.identifier, for: indexPath) as? weekWeatherCollectionViewCell else {return UICollectionViewCell()}
+            
+            item.bindData(data: weekWeatherItemData[indexPath.row])
+            return item
             
         default:
-            return UITableViewCell()
+            return UICollectionViewCell()
+        }
+    }
+}
+
+extension DetailViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.section {
+        case 0:
+            let itemWidthOrHeight: CGFloat = 212
+            return CGSize(width: collectionView.frame.width, height: itemWidthOrHeight)
+        case 1:
+            let itemWidthOrHeight: CGFloat = 146
+            return CGSize(width: collectionView.frame.size.width, height: itemWidthOrHeight)
+        case 2:
+            let itemWidthOrHeight: CGFloat = 55
+            return CGSize(width: collectionView.frame.width, height: itemWidthOrHeight)
             
+        default:
+            return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let sectionInsets = UIEdgeInsets.zero
+        switch section {
+        case 0:
+            return UIEdgeInsets(top: 34, left: 0, bottom: 44, right: 0)
+        case 1:
+            return UIEdgeInsets(top: 0, left: 8, bottom: 16, right: 0)
+        case 2:
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        default:
+            break
+        }
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch indexPath.section {
+        case 1:
+            
+            if kind == UICollectionView.elementKindSectionHeader {
+                guard let secondSectionHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: dayWeatherCollectionViewHeaderView.identifier, for: indexPath) as? dayWeatherCollectionViewHeaderView else {
+                    return UICollectionReusableView()
+                }
+                return secondSectionHeaderView
+            }
+            
+        case 2:
+            if kind == UICollectionView.elementKindSectionHeader {
+                guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: weekWeatherCollectionViewHeaderView.identifier, for: indexPath) as? weekWeatherCollectionViewHeaderView else {
+                    return UICollectionReusableView()
+                }
+                return headerView
+            }
+            
+            else if kind == UICollectionView.elementKindSectionFooter {
+                guard let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: weekWeatherCollectionViewFooterView.identifier, for: indexPath) as? weekWeatherCollectionViewFooterView else {
+                    return UICollectionReusableView()
+                }
+                return footerView
+            }
+            
+        default:
+            return UICollectionReusableView()
+        }
+        
+        return UICollectionReusableView()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        switch section {
+        case 1:
+            return CGSize(width: collectionView.frame.width - 40, height: 70)
+        case 2:
+            return CGSize(width: collectionView.frame.width - 40, height: 38)
+        default:
+            return CGSize(width: 0, height: 0)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        switch section {
+        case 2:
+            return CGSize(width: collectionView.frame.width - 40, height: 32)
+        default:
+            return CGSize(width: 0, height: 0)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
+
+
+
