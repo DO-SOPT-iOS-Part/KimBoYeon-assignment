4dd26d75da7f2ee007a723a357867b9c45603371
boyeon <judy010119@gmail.com>
2023-11-10 16:37:41 +0900

    [Add] 섹션 3의 헤더뷰 추가

--- weatherApp/weatherApp/Presentation/Detail/weekWeatherCollectionViewFooterView.swift
+++ weatherApp/weatherApp/Presentation/Detail/weekWeatherCollectionViewFooterView.swift
@@ -5,8 +5,40 @@
 //  Created by 김보연 on 11/8/23.
 //
 
-import Cocoa
+import UIKit
 
-class weekWeatherCollectionViewFooterView: UITableViewHeaderFooterView {
+import SnapKit
+import Then
 
+class weekWeatherCollectionViewFooterView: UICollectionReusableView {
+    
+    static let identifier = "CustomCollectionFooterView"
+    
+    private let footerContainerView = UIView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setupFooterView()
+        configureLayout()
+    }
+    
+    func setupFooterView() {
+        
+        footerContainerView.do {
+            $0.backgroundColor = .clear
+            $0.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.03).cgColor
+        }
+    }
+    
+    func configureLayout() {
+        
+        addSubview(footerContainerView)
+        
+        footerContainerView.snp.makeConstraints {
+            $0.height.equalTo(32)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+    }
 }
