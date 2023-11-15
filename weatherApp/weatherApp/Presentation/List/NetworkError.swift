a29de717ff3c341504a91c486546680b9c878dd6
boyeon <judy010119@gmail.com>
2023-11-16 01:01:09 +0900

    [Add] Error 파일 추가

--- weatherApp/weatherApp/Presentation/List/NetworkError.swift
+++ weatherApp/weatherApp/Presentation/List/NetworkError.swift
@@ -5,4 +5,25 @@
 //  Created by 김보연 on 11/14/23.
 //
 
-import Foundation
+enum NetworkError: Int, Error, CustomStringConvertible {
+    var description: String { self.errorDescription }
+    case requstEncodingError
+    case responseDecodingError
+    case responseError
+    case unknownError
+    case loginFailed = 400
+    case internalServerError = 500
+    case notFoundError = 404
+    
+    var errorDescription: String {
+        switch self {
+        case .loginFailed: return "로그인에 실패하였습니다."
+        case .requstEncodingError: return "REQUEST_ENCODING_ERROR"
+        case .responseError: return "RESPONSE_ERROR"
+        case .responseDecodingError: return "RESPONSE_DECODING_ERROR"
+        case .unknownError: return "UNKNOWN_ERROR"
+        case .internalServerError: return "500:INTERNAL_SERVER_ERROR"
+        case .notFoundError: return "404:NOT_FOUND_ERROR"
+        }
+    }
+}
