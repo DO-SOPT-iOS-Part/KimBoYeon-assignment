c51a42ee3a4a972ec5dc8420b6bd58daa29867a7
boyeon <judy010119@gmail.com>
2023-11-16 01:05:17 +0900

    [Add] DataModel 파일 추가

--- weatherApp/weatherApp/Presentation/List/WeatherInfoDataModel.swift
+++ weatherApp/weatherApp/Presentation/List/WeatherInfoDataModel.swift
@@ -1,8 +1,46 @@
 //
-//  weatherInfoDataModel.swift
+//  WeatherInfoDataModel.swift
 //  weatherApp
 //
 //  Created by 김보연 on 11/14/23.
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct WeatherInfoDataModel: Codable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+    let timezone: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case main
+        case name = "name"
+        case timezone
+    }
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, tempMin, tempMax: Double
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+    
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let main: String
+    
+    enum CodingKeys: CodingKey {
+        case main
+    }
+}
+
+
