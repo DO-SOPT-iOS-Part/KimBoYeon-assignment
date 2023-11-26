4b9dad91ed5a6484ad46116bd7d9becbf6fb5b00
boyeon <judy010119@gmail.com>
2023-11-27 04:19:29 +0900

    [Setting] #4 - Add Literals files

--- weatherApp/weatherApp/Global/Literals/FontLiterals.swift
+++ weatherApp/weatherApp/Global/Literals/FontLiterals.swift
@@ -5,4 +5,41 @@
 //  Created by 김보연 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+enum AppFontName: String {
+    
+    case regularFont = "SFProText-Regular"
+    case mediumFont = "SFProText-Medium"
+    case boldFont = "SFProText-Bold"
+    case thinFont = "SFProText-Thin"
+    case lightFont = "SFProText-Light"
+    
+    var name: String {
+        return self.rawValue
+    }
+}
+
+extension UIFont {
+
+    class func regular(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.regularFont.rawValue, size: size)!
+    }
+
+    class func medium(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.mediumFont.rawValue, size: size)!
+    }
+
+    class func bold(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.boldFont.rawValue, size: size)!
+    }
+    
+    class func light(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.lightFont.rawValue, size: size)!
+    }
+    
+    class func thin(size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.thinFont.rawValue, size: size)!
+    }
+}
+
--- weatherApp/weatherApp/Global/Literals/SizeLiterals.swift
+++ weatherApp/weatherApp/Global/Literals/SizeLiterals.swift
@@ -5,4 +5,13 @@
 //  Created by 김보연 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+struct SizeLiterals {
+    
+    struct Screen {
+        static let screenWidth: CGFloat = UIScreen.main.bounds.width
+        static let screenHeight: CGFloat = UIScreen.main.bounds.height
+        static let deviceRatio: CGFloat = screenWidth / screenHeight
+    }
+}
