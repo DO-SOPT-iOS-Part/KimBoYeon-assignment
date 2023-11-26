5c0135b239f44dc315ff3e4e8fde4e1ca828fdc7
boyeon <judy010119@gmail.com>
2023-11-27 04:18:23 +0900

    [Setting] #4 - Add BaseView file

--- weatherApp/weatherApp/Presentation/Base/BaseView.swift
+++ weatherApp/weatherApp/Presentation/Base/BaseView.swift
@@ -5,4 +5,32 @@
 //  Created by 김보연 on 11/27/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class BaseView: UIView {
+
+    private lazy var viewName = self.className
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+        print("🧵 \(viewName) has been successfully Removed")
+    }
+    
+    /// View 의 Style 을 set 합니다.
+    func setUI() {}
+    /// View 의 Layout 을 set 합니다.
+    func setLayout() {}
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
