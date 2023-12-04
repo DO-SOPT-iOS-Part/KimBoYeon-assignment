bc6face6400e7a406779d5f8aea2f851ab4503a9
boyeon <judy010119@gmail.com>
2023-12-04 20:59:19 +0900

    [Delete] #6 - UI 요소 ListView에 이동

--- weatherApp/weatherApp/Presentation/List/ViewControllers/ListViewController.swift
+++ weatherApp/weatherApp/Presentation/List/ViewControllers/ListViewController.swift
@@ -10,138 +10,43 @@ import UIKit
 import SnapKit
 import Then
 
-class ListViewController: UIViewController {
+class ListViewController: BaseViewController {
     
-
-    private let listCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
-
-    private let searchController = UISearchController(searchResultsController: nil)
-    private var filteredWeatherList: [WeatherCardItemData] = []
-
-    
-    
-    @objc
-    func locationListTapped() {
-        pushToViewController()
-    }
-    
+    private let listView = ListView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        
-        title = "날씨"
-        
-        
-        let editerButton = UIButton(type: .custom)
-        editerButton.setImage(UIImage(named: "editer"), for: .normal)
-        
-        let customBarButtonItem = UIBarButtonItem(customView: editerButton)
-        navigationItem.rightBarButtonItem = customBarButtonItem
-        
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationItem.largeTitleDisplayMode = .always
-        self.navigationController?.navigationBar.barTintColor = .black
-        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
-        self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        
-        navigationItem.searchController = searchController
-        searchController.searchResultsUpdater = self
-        navigationItem.hidesSearchBarWhenScrolling = false
-        definesPresentationContext = false
-        
-        self.navigationController?.navigationBar.isHidden = false
-        
-        setCollectionViewConfig()
-        setStyle()
-        setLayout()
-        
-        Task {
-            await setWeatherInfo()
-        }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.isHidden = false
+    override func loadView() {
+        self.view = listView
     }
     
-    private func setStyle() {
-        listCollectionView.do {
-            $0.backgroundColor = .black
-            let flowLayout = UICollectionViewFlowLayout()
-            flowLayout.scrollDirection = .vertical
-            $0.collectionViewLayout = flowLayout
-        }
-        
-        searchController.do {
-            $0.searchBar.placeholder = "도시 또는 공항 검색"
-            $0.searchBar.searchBarStyle = .minimal
-            $0.obscuresBackgroundDuringPresentation = false
-            $0.hidesNavigationBarDuringPresentation = false
-            $0.searchBar.searchTextField.textColor = .white
-        }
+    override func setUI() {
+        view.backgroundColor = .black
     }
     
-    func convertTime(timezone: Int) -> String {
-        let timeZone = TimeZone(secondsFromGMT: timezone)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm"
-        dateFormatter.timeZone = timeZone
-        let currentDate = Date()
-        let formattedTime = dateFormatter.string(from: currentDate)
-        return formattedTime
+    override func setDelegates() {
+        listView.listCollectionView.delegate = self
+        listView.listCollectionView.dataSource = self
     }
     
-    private func setWeatherInfo() async {
-        let locationData = ["gongju", "suwon", "gumi", "iksan", "daegu", "cheongju", "mokpo", "busan", "seosan", "seoul"]
-        for i in locationData {
-            do {
-                let status = try await GetInfoService.shared.PostRegisterData(name: i)
-                let getInfo: WeatherCardItemData = WeatherCardItemData(myLocationLabel: status.name,
-                                                                       myLocationNameLabel:
-                                                                        convertTime(timezone: status.timezone),
-                                                                       myLocationConditionLabel: status.weather[0].main,
-                                                                       myLocationAverageTemperatureLabel: Int(status.main.temp),
-                                                                       myLocationMinimumTemperatureLabel: Int(status.main.tempMin),
-                                                                       myLocationMaximumTemperatureLabel: Int(status.main.tempMax))
-                weatherList.append(getInfo)
-            } catch {
-                print(error)
-            }
-        }
-        self.listCollectionView.reloadData()
+    override func setRegister() {
+        listView.listCollectionView.register(ListCardCollectionViewCell.self,
+                                             forCellWithReuseIdentifier: ListCardCollectionViewCell.identifier)
     }
     
-    private func setLayout() {
-        self.view.addSubview(listCollectionView)
-        
-        listCollectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-
-    }
-    
-    private func setCollectionViewConfig() {
-        self.listCollectionView.register(weatherListCollectionCell.self,
-                                         forCellWithReuseIdentifier: weatherListCollectionCell.identifier)
-        self.listCollectionView.delegate = self
-        self.listCollectionView.dataSource = self
-    }
-    
-    @objc func pushToViewController() {
-        let DetailViewController = DetailViewController()
-        self.navigationController?.pushViewController(DetailViewController, animated: true)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
     }
 }
 
 extension ListViewController: UICollectionViewDelegate {}
+
 extension ListViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ listCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return isSearchActive ? filteredWeatherList.count : weatherList.count
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -150,64 +55,9 @@ extension ListViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ listCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let item = listCollectionView.dequeueReusableCell(withReuseIdentifier: weatherListCollectionCell.identifier,
-                                                                for: indexPath) as? weatherListCollectionCell else {return UICollectionViewCell()}
+        guard let item = listCollectionView.dequeueReusableCell(withReuseIdentifier: ListCardCollectionViewCell.identifier,
+                                                                for: indexPath) as? ListCardCollectionViewCell else {return UICollectionViewCell()}
         item.bindData(data: weatherList[indexPath.row])
-        return item
-    }
-
-    private var isSearchActive: Bool {
-        return searchController.isActive && !isSearchBarEmpty
-    }
-    
-    private var isSearchBarEmpty: Bool {
-        return searchController.searchBar.text?.isEmpty ?? true
-    }
-}
-
-extension ListViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width - 40, height: 117)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return CGFloat(16)
-    }
-}
-
-extension ListViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text?.lowercased(), !text.isEmpty else {
-            filteredWeatherList = weatherList
-            listCollectionView.reloadData()
-            return
-        }
-        
-        filteredWeatherList = weatherList.filter {
-            $0.myLocationLabel.lowercased().contains(text)
-        }
-        
-        listCollectionView.reloadData()
-    }
-}
-
-extension ListViewController: UICollectionViewDelegate {}
-extension ListViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ listCollectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return weatherCardItemData.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let selectedWeatherItem = weatherCardItemData[indexPath.row]
-        let detailViewController = DetailViewController()
-        navigationController?.pushViewController(detailViewController, animated: true)
-    }
-    
-    func collectionView(_ listCollectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let item = listCollectionView.dequeueReusableCell(withReuseIdentifier: weatherListCollectionCell.identifier,
-                                                                for: indexPath) as? weatherListCollectionCell else {return UICollectionViewCell()}
-        item.bindData(data: weatherCardItemData[indexPath.row])
         return item
     }
 }
