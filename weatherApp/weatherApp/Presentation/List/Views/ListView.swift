57895f17a7f708fa9f8c7ddb7237a1c69b1f85f6
boyeon <judy010119@gmail.com>
2023-12-04 20:56:38 +0900

    [Fix] #6 - VC에 있던 UI 이동 및 정리

--- weatherApp/weatherApp/Presentation/List/Views/ListView.swift
+++ weatherApp/weatherApp/Presentation/List/Views/ListView.swift
@@ -1,5 +1,5 @@
 //
-//  CardView.swift
+//  ListView.swift
 //  weatherApp
 //
 //  Created by 김보연 on 11/27/23.
@@ -10,23 +10,32 @@ import UIKit
 import SnapKit
 import Then
 
-class CardView: BaseView {
+class ListView: BaseView {
 
-    private lazy var viewName = self.className
+    let listCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
     
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setUI() {
-        
+        listCollectionView.do {
+            $0.backgroundColor = .black
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .vertical
+            $0.collectionViewLayout = flowLayout
+        }
     }
     
     override func setLayout() {
+        self.addSubview(listCollectionView)
         
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        listCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
